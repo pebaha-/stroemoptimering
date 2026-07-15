b62b3427f1bb8e1139380bc6721041a793fa0f38
pebaha- <pebaha@gmail.com>
2026-07-15 23:27:26 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StromligningApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
