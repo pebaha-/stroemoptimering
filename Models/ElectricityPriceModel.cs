109ab2fc21add131e4f1a93cbf2d881d1217be4a
pebaha- <pebaha@gmail.com>
2026-07-17 20:17:54 +0200

    Add model class

---
+++ Models/ElectricityPriceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StromligningApp.Models
+{
+    public sealed class ElectricityPrice
+    {
+        public DateTimeOffset StartTime { get; init; }
+
+        public DateTimeOffset EndTime { get; init; }
+
+        public decimal PricePerKwh { get; init; }
+    }
+}
