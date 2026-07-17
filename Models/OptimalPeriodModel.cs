8509a4457c4410d132aad8a8d48688a7a9ee1d0e
pebaha- <pebaha@gmail.com>
2026-07-18 01:12:16 +0200

    Use Europe/Copenhagen as timezone in model

--- Models/OptimalPeriodModel.cs
+++ Models/OptimalPeriodModel.cs
@@ -1,11 +1,23 @@
-﻿namespace StromligningApp.Models
+﻿namespace StromligningApp.Models;
+
+public sealed class OptimalPeriodModel
 {
-    public sealed class OptimalPeriodModel
-    {
-        public DateTimeOffset StartTime { get; init; }
+    public DateTimeOffset StartTime { get; init; }
 
-        public DateTimeOffset EndTime { get; init; }
+    public DateTimeOffset EndTime { get; init; }
 
-        public decimal AveragePricePerKwh { get; init; }
-    }
+    public decimal AveragePricePerKwh { get; init; }
+
+    public DateTimeOffset LocalStartTime =>
+        TimeZoneInfo.ConvertTime(
+            StartTime,
+            DanishTimeZone);
+
+    public DateTimeOffset LocalEndTime =>
+        TimeZoneInfo.ConvertTime(
+            EndTime,
+            DanishTimeZone);
+
+    private static readonly TimeZoneInfo DanishTimeZone =
+        TimeZoneInfo.FindSystemTimeZoneById("Europe/Copenhagen");
 }
